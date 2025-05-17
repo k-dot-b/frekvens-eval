bba35fb7e6bf8e2b9ad97134ccba98669455c899
kagoston02 <kiss.agoston0202@gmail.com>
2025-05-17 20:31:58 +0200

    mergesort complete

--- sorting.cpp
+++ sorting.cpp
@@ -8,7 +8,6 @@ static const uint16_t columnHeight[] = {0x0001, 0x0003, 0x0007, 0x1f, 0x3f, 0x7f
 0x7ff, 0xfff, 0x1fff, 0x3fff, 0x7fff, 0xffff};
 */
 
-static const int N = 16;
 //static const long RAND_MAX = 2047;
 
 
@@ -46,7 +45,8 @@ void mergeSort(int bottom, int top)
                 tmp[k++] = numbers[i++];
             else
                 tmp[k++] = numbers[j++];
-            bitMapGenerate();
+            //bitMapGenerate();
+            bitMapGenerInt(tmp);
             mrefresh(g_bitmap, DIMC, 8, LATCH_PIN, OE_PIN);
             delay(SORT_STEP_DELAY);
         }
@@ -54,18 +54,21 @@ void mergeSort(int bottom, int top)
         while (i <= mid)
             tmp[k++] = numbers[i++];
             bitMapGenerate();
+            //bitMapGenerInt(tmp);
             mrefresh(g_bitmap, DIMC, 8, LATCH_PIN, OE_PIN);
             delay(SORT_STEP_DELAY);
 
         while (j <= top)
             tmp[k++] = numbers[j++];
             bitMapGenerate();
+            //bitMapGenerInt(tmp);
             mrefresh(g_bitmap, DIMC, 8, LATCH_PIN, OE_PIN);
             delay(SORT_STEP_DELAY);
 
         for (i = bottom, k = 0; i <= top; ++i, ++k)
             numbers[i] = tmp[k];
             bitMapGenerate();
+            //bitMapGenerInt(tmp);
             mrefresh(g_bitmap, DIMC, 8, LATCH_PIN, OE_PIN);
             delay(SORT_STEP_DELAY);
     }
@@ -105,5 +108,15 @@ void bitMapGenerate(){
   }
 }
 
+void bitMapGenerInt(int array[N]){
+
+  for(int i=0; i<N; i++){   //column
+    for(int j=0; j<N; j++){ //row 
+        if(j < array[i]){
+            g_bitmap[i][j] = 1;
+        } else g_bitmap[i][j] = 0;
+    }
+  }
+}
 
 
--- sorting.h
+++ sorting.h
@@ -3,13 +3,14 @@
 #include <Arduino.h>
 #include "shift.h"
 
-
+#define N 16
 
 extern const int LATCH_PIN;
 extern const int OE_PIN;
 
 void generate();
 void sorting();
-void shuffle(uint8_t array[16]);
+void shuffle(uint8_t array[N]);
 void mergeSort(int, int);
 void bitMapGenerate();
+void bitMapGenerInt(int array[N]);
